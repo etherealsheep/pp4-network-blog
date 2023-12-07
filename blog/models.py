ce6243199da27ef82f8e34463b42b3325a03966b
etherealsheep <g.perrotta79@gmail.com>
2023-12-07 02:30:53 +0000

    modify Posts created_on adding new field

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now=False, auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
 
