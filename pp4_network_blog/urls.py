d8aaaf239cd95764abe0c448186d666fcd3b4378
etherealsheep <g.perrotta79@gmail.com>
2023-12-07 03:50:21 +0000

    fix urls files

--- pp4_network_blog/urls.py
+++ pp4_network_blog/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path(r'^', include ('blog.urls')),
+    path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
 ]
