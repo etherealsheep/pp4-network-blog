a8fdfdabd3b0c75d27fbcb7f1bf5c86283bea49a
etherealsheep <g.perrotta79@gmail.com>
2023-12-13 23:07:21 +0000

    fix typos in test_models.py

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -7,41 +7,41 @@ class TestModels(TestCase):
 
     def test_post_status_defaults_to_draft(self):
         user = User.objects.create(username='testname')
-        recipe = Post.objects.create(
-            title='post',
+        post = Post.objects.create(
+            title='post1',
             author=user
         )
-        self.assertFalse(recipe.status)
+        self.assertFalse(post.status)
 
     def test_comment_approved_defaults_to_false(self):
         user = User.objects.create(username='testname')
-        recipe = Post.objects.create(
-            title='post',
+        post = Post.objects.create(
+            title='post1',
             author=user
         )
         comment = Comment.objects.create(
-            post=recipe,
+            post=post,
             body='post comment',
         )
         self.assertFalse(comment.approved)
 
     def test_post_string_method_returns_title(self):
         user = User.objects.create(username='testname')
-        recipe = Post.objects.create(
-            title='post',
+        post = Post.objects.create(
+            title='post1',
             author=user
         )
-        self.assertEqual(str(recipe), 'recipe1')
+        self.assertEqual(str(post), 'post1')
 
     def test_comment_string_method_returns_comment(self):
         user = User.objects.create(username='testname')
-        recipe = Post.objects.create(
-            title='post',
+        post = Post.objects.create(
+            title='post1',
             author=user
         )
         comment = Comment.objects.create(
-            post=recipe,
+            post=post,
             name=user,
             body='post comment',
         )
-        self.assertEqual(str(comment), 'Comment post by testname')
+        self.assertEqual(str(comment), 'Comment post comment by testname')
